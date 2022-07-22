89c0db1e5e5a66dea23bca8693270f00db129a35
FrugalCarrot7 <james.devine1995@gmail.com>
2022-07-22 15:20:39 -0700

    adds psql database and models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.forms import CharField, DateField, IntegerField
 
 # Create your models here.
+
+class House(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Room(models.Model):
+    name = models.CharField(max_length=50)
+    size = models.PositiveIntegerField()
+
+class Item(models.Model):
+    name = models.CharField(max_length=50)
+    date_purchase = models.DateField()
+    price = models.IntegerField()
+    description = CharField(max_length=200)
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=50)
+    due = models.DateField()
+    instructions = models.CharField(max_length=500)
+
